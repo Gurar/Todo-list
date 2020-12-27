36a2480c09a8f35a6744a4d9e6aafe61a55864eb
Gurar <jozyk@inbox.lt>
2020-12-28 00:45:04 +0200

    fix

--- js/main.js
+++ js/main.js
@@ -1,17 +1,8 @@
 import {Todo} from './components/Todo.js';
-import {formGenerate} from './components/formGenerate.js';
 
 const todo = new Todo({
     selector: 'main'
 });
-
-// const addForm = {
-//     selector: '.content',
-//     title: 'gurraAdd new task',
-//     action: 'add'
-// }
-
-// formGenerate(addForm);
 
 todo.init();
 
