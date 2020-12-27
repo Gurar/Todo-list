b1568e477be7242b00d6ca02023313dd6da05fa4
Gurar <jozyk@inbox.lt>
2020-12-28 00:23:12 +0200

    todo list

---
+++ js/components/Todo.js
@@ -0,0 +1,183 @@
+class Todo {
+    constructor(params) {
+        this.selector = params.selector;
+        this.DOM = null;
+        this.taskList = [];
+        this.addForm = {
+            selector: '.content',
+            title: 'Add new task',
+            action: 'add'
+        }
+    }
+
+    init() {
+        if(!this.isValidSelector()) {
+            return false
+        }
+        this.updateTask();
+        this.render();
+        this.events();
+    }
+
+    isValidSelector() {
+        const DOM = document.querySelector(this.selector);
+        if(!DOM) {
+            return false;
+        }
+        this.DOM = DOM;
+        return true;
+    }
+
+    
+
+    generateItem(task) {
+        return ` <div class="item">
+                    <p>${task.text}</p>
+                        <div class="actions">
+                            <div class="btn small edit">Edit</div>
+                            <div class="btn small remove">Remove</div>
+                        </div>
+                </div>`;
+    }
+
+    generateForm(params) {
+        
+        const selector = document.querySelector(params.selector);
+        const form = `<form action="">
+                    <h2>${params.title}</h2>
+                        <textarea></textarea>
+                        <button class="btn cancel" type="submit">Cancel</button>
+                        <button class="btn ${params.action}" type="submit" data-action=${params.action}>${params.action}</button>
+                    </form>`;
+
+        selector.innerHTML = form;
+    }
+
+    renderList() {
+        let HTML = '';
+        for(let item of this.taskList) {
+            HTML += this.generateItem(item);
+        }
+
+        this.DOM.innerHTML = HTML;
+        this.addEvents();
+    }
+
+    render() {
+        this.renderList();
+        this.generateForm(this.addForm);
+    }
+
+    events() {
+        const addNewTask = document.querySelector('.add-new');
+        const lighbox = document.querySelector('.lighbox');
+        const form = lighbox.querySelector('form');
+        const textarea = form.querySelector('textarea');
+        const btnCancel = form.querySelector('.cancel');
+        const btnAdd = form.querySelector('.add');
+
+        addNewTask.addEventListener('click', () =>{
+            lighbox.classList.add('show');
+        });
+        
+        addEventListener('keyup', ({key}) => {
+           if(key === 'Escape') {
+               lighbox.classList.remove('show');
+           }
+        })
+        
+        btnCancel.addEventListener('click', (e) => {
+            e.preventDefault();
+            lighbox.classList.remove('show');
+        });
+        
+        btnAdd.addEventListener('click', (e) => {
+            e.preventDefault();
+            const task = {
+                text: textarea.value,
+                isCompleted: false
+            }
+            
+            this.addTask(task);
+            textarea.value = '';
+            lighbox.classList.remove('show');
+        });
+    }
+
+    addTask(task) {
+        this.taskList.push(task);
+        this.renderList();
+        return true;
+    }
+
+    deleteTask(taskIndex) {
+        this.taskList = this.taskList.filter((item, index) => index !== taskIndex);
+        this.renderList();
+    }
+
+    addEvents() {
+        const items = this.DOM.querySelectorAll('.item');
+
+        for (let i = 0; i < items.length; i++) {
+            const item = items[i];
+            const editBtn = item.querySelector('.btn.edit');
+            const removeBtn = item.querySelector('.btn.remove');
+
+            editBtn.addEventListener('click', () => {
+                this.initTododItemEditing(i);
+            })
+            removeBtn.addEventListener('click', () => {
+                this.deleteTask(i);
+            })
+        }
+    }
+
+    updateTask() {
+        if(!this.DOM.classList.contains('list')) {
+            this.DOM.classList.add('list');
+        }
+    }
+
+    initTododItemEditing(taskIndex) {
+        const lighbox = document.querySelector('.lighbox');
+        const editForm = {
+            selector: '.content',
+            title: 'edit task',
+            action: 'edit'
+        }
+
+        this.generateForm(editForm);
+
+        lighbox.classList.add('show');
+
+        const textarea = document.querySelector('textarea');
+        const btnCancel = document.querySelector('button.cancel');
+        const btnEdit= document.querySelector('button.edit');
+
+    
+        btnCancel.addEventListener('click', (e) => {
+            e.preventDefault();
+            lighbox.classList.remove('show');
+            this.generateForm(this.addForm);
+            this.events();
+        });
+
+        btnEdit.addEventListener('click', (e) => {
+            e.preventDefault();
+            this.taskList[taskIndex] = {
+                text: textarea.value,
+                isCompleted: false
+            }
+
+            this.renderList();
+            lighbox.classList.remove('show');
+            this.generateForm(this.addForm);
+            this.events();
+        });
+
+
+     
+    }
+}
+
+export {Todo};
--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
-console.log('hello todo');
+import {Todo} from './components/Todo.js';
+import {formGenerate} from './components/formGenerate.js';
+
+const todo = new Todo({
+    selector: 'main'
+});
+
+// const addForm = {
+//     selector: '.content',
+//     title: 'gurraAdd new task',
+//     action: 'add'
+// }
+
+// formGenerate(addForm);
+
+todo.init();
+
+
+
+
+
+
